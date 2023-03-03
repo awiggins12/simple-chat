5aecc82f9351b4249b2e79ddc231d396da4412d2
awiggins12 <35470874+awiggins12@users.noreply.github.com>
2023-03-02 20:52:25 -0500

    File saving, clear input box, UI tweaks

--- simple-chat.py
+++ simple-chat.py
@@ -3,6 +3,7 @@ import openai
 import os
 import json
 import glob
+import datetime
 
 openai.api_key = ""
 messages = []
@@ -11,22 +12,20 @@ save_dir = "output"
 
 """
 TODO
-1. Re-generate response - DONE
-2. Saving to a different file name
-3. Auto-clear the submit box?  
-4. Better placeholder text and labels for the system/content
-5. Improve UI
-6. Convert the output to html?  Then convert markdown to html
-7. Edit Response
-8. Save re-generated responses
-9. Scroll to end of output
-10. Improve error handling of files
+* Auto-clear the submit box?  
+* Better placeholder text and labels for the system/content
+* Improve UI
+* Convert the output to html?  Then convert markdown to html
+* Edit Response
+* Save re-generated responses
+* Scroll to end of output
+*  Improve error handling of files
 """
 
 
 def clearChat():
     messages.clear()
-    return None
+    return [None, get_new_filename()]
     
 def load_save_file(value):
     test = []
@@ -50,14 +49,14 @@ def getSaveFiles():
         file_names.append(file)
     return file_names
 
-def regenerate_response(Context, Content, file_name, autosave):
+def regenerate_response(context, content, file_name, autosave, autoclear):
     #if the last element in the array is assistant, remove it.
     if len(messages) > 0 and messages[-1]["role"] == "assistant":
         messages.pop()
         #now check to see if the next to last message is from user, we want to remove that now.
         if len(messages) > 0 and messages[-1]["role"] == "user":
             messages.pop()
-    return chat(Context, Content, file_name, autosave)
+    return chat(context, content, file_name, autosave, autoclear)
 
 def format_message_data():
     chat_history = ""
@@ -65,15 +64,20 @@ def format_message_data():
         if message["role"] != "system":
             chat_history += message["role"] + ": " + message["content"].lstrip('\n') + "\n\n"
     return chat_history
-
-def chat(Context, Content, file_name, autosave):
-    if Context:
-        if len(messages) > 0 and messages[0]["role"] == "system":
-            messages[0]["content"] = Context
-        else:
-            messages.insert(0, {"role": "system", "content": Context})
     
-    messages.append({"role": "user", "content": Content})
+def get_new_filename():
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%H%M%S")
+    return formatted_time + "_Default.txt"
+
+def chat(context, content, file_name, autosave, autoclear):
+    if context:
+        if len(messages) > 0 and messages[0]["role"] == "system":
+            messages[0]["content"] = context
+        else:
+            messages.insert(0, {"role": "system", "content": context})
+    
+    messages.append({"role": "user", "content": content})
 
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
@@ -84,18 +88,19 @@ def chat(Context, Content, file_name, autosave):
     messages.append(response['choices'][0]['message'])
     
     if autosave:
-        # Extract the directory path from the full path and filename
-        save_directory = os.path.dirname(savepath)
         
         #Check to make sure the directory exists
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
         
         #Save the messages to file
-        with open(savepath, 'w') as f:
+        with open(save_dir + '/' + file_name, 'w') as f:
             f.write(json.dumps(messages))
     
-    return format_message_data()
+    if autoclear:
+            content = ""
+    
+    return [format_message_data(), content]
     
 with gr.Blocks() as demo:
     output = gr.Textbox(label="Output Box", value=format_message_data)
@@ -105,21 +110,21 @@ with gr.Blocks() as demo:
         with gr.Column(scale=2):
             file_dropdown = gr.Dropdown(getSaveFiles(), label="Load File")
         with gr.Column(scale=2):            
-            file_name = gr.Textbox(label="Save file name")
+            file_name = gr.Textbox(label="Save file name", value=get_new_filename)
         with gr.Column(scale=1):
-            autosave = gr.Checkbox(label="Autosave")
+            autosave = gr.Checkbox(label="Autosave", value=True)
             autoclear = gr.Checkbox( label="Auto-clear input", value=True)
     
-    Context = gr.Textbox(lines=1, placeholder="Enter information to instruct how inputs stuff...")
-    Content = gr.Textbox(lines=5, placeholder="Text...")
+    context = gr.Textbox(lines=1, placeholder="Enter information to instruct how inputs stuff...")
+    content = gr.Textbox(lines=5, placeholder="Text...")
     with gr.Row(variant="panel"):
         with gr.Column(scale=30):
             submit = gr.Button("Submit", variant='primary')
         with gr.Column(scale=1):
             regenerate = gr.Button(value="regen")
-    submit.click(fn=chat, inputs=[Context, Content, file_name, autosave], outputs = output)
-    regenerate.click(fn=regenerate_response, inputs=[Context, Content, file_name, autosave], outputs = output)
-    clear.click(fn=clearChat,inputs=None, outputs = output)
-    file_dropdown.change(fn=load_save_file, inputs=file_dropdown, outputs=[output, Context, file_name, file_dropdown])
+    submit.click(fn=chat, inputs=[context, content, file_name, autosave, autoclear], outputs = [output, content])
+    regenerate.click(fn=regenerate_response, inputs=[context, content, file_name, autosave, autoclear], outputs = [output, content])
+    clear.click(fn=clearChat,inputs=None, outputs = [output, file_name], show_progress=False)
+    file_dropdown.change(fn=load_save_file, inputs=file_dropdown, outputs=[output, context, file_name, file_dropdown])
 
 demo.launch()
