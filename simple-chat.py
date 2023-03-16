4b491b351579e3f64cd53b535c6929f223ec2f0c
metalpuppy <no@no.com>
2023-03-16 00:10:58 -0400

    adding a slider to trim messages to api

--- simple-chat.py
+++ simple-chat.py
@@ -80,12 +80,12 @@ def get_save_files():
         file_names.append(file)
     return file_names
 
-def regenerate_response(context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty):
+def regenerate_response(context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty, historyLength):
     #if the last element in the array is assistant, remove it.
     if len(messages) > 0 and messages[-1]["role"] == "assistant":
         messages.pop()
 
-    return chat(context, "", file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty)
+    return chat(context, "", file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty, historyLength)
 
 def format_message_data():
     
@@ -104,8 +104,23 @@ def get_new_filename():
     formatted_time = now.strftime("%H%M%S")
     return formatted_time + "_Default.txt"
 
-def chat(context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty):
+def chat(context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty, historyLength):
+
+    payload = []
+
+    # strip out the system message if it is present
+    for message in messages:
+        if message['role'] != 'system':
+            payload.append(message)
+
+    if historyLength > len(payload):
+        historyLength = len(payload)
+    payload = payload[-historyLength:]
+
     if context:
+        # only send 'system' message if currently populated
+        payload.insert(0, {"role": "system", "content": context})
+
         if len(messages) > 0 and messages[0]["role"] == "system":
             messages[0]["content"] = context
         else:
@@ -113,10 +128,11 @@ def chat(context, content, file_name, autosave, autoclear, max_length, temperatu
     
     if len(content) > 0:
         messages.append({"role": "user", "content": content})
+        payload.append({"role": "user", "content": content})
 
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
-      messages=messages,
+      messages=payload,
       temperature=temperature,
       top_p=top_p,
       frequency_penalty=frequency_penalty,
@@ -124,6 +140,9 @@ def chat(context, content, file_name, autosave, autoclear, max_length, temperatu
       #max_tokens=max_length
     )
     
+    print(f"Tokens used: {response['usage']['total_tokens']}")
+    print(f"Finish reason: {response['choices'][0]['finish_reason']}")
+
     #add the openai response to the memory
     messages.append(response['choices'][0]['message'])
     
@@ -206,10 +225,13 @@ with gr.Blocks(css=css, title="Simple Chat") as demo:
             with gr.Column():
                 frequency_penalty = gr.Slider(minimum=-2, maximum=2, step=0.01, label="Frequency Penalty", value=0, interactive=True)
                 presence_penalty = gr.Slider(minimum=-2, maximum=2, step=0.01, label="Presence Penalty", value=0, interactive=True)
+        with gr.Row():
+            # controls the number of messages in history to include in the context. A hacky way to avoid exceeding the token limit.
+            historyLength = gr.Slider(label='Message History Limit', minimum=1, maximum=50, step=1, value=30,)
         
     #Bindings
-    submit.click(fn=chat, inputs=[context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty], outputs = [output, content, file_dropdown])
-    regenerate.click(fn=regenerate_response, inputs=[context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty], outputs = [output, content, file_dropdown])
+    submit.click(fn=chat, inputs=[context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty, historyLength], outputs = [output, content, file_dropdown])
+    regenerate.click(fn=regenerate_response, inputs=[context, content, file_name, autosave, autoclear, max_length, temperature, top_p, frequency_penalty, presence_penalty, historyLength], outputs = [output, content, file_dropdown])
     clear.click(fn=clear_chat,inputs=None, outputs = [output, file_name], show_progress=False)
     file_dropdown.change(fn=load_save_file, inputs=[file_dropdown, context, file_name], outputs=[output, context, file_name, file_dropdown])
     submit_api_key.click(fn=set_new_api_key, inputs=api_key_box, outputs=api_key_setup)
